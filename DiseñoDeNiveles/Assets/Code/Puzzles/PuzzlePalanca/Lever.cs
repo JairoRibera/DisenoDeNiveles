09cd47c95e90bb6666ce1f6d3f1ab7c7134e98bf
JairoRibera <jairosalvatierra098@gmail.com>
2024-12-03 12:50:01 +0100

    Puzzle completados

---
+++ DiseñoDeNiveles/Assets/Code/Puzzles/PuzzlePalanca/Lever.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    private Animator animPalanca;
+    public Lever[] palancaDesactivar = new Lever[4];
+    public Lever[] palancaActivar = new Lever[4];
+    public bool isActive;
+    public bool canInteract;
+    private LeverCodeManager _pCM;
+    private void Start()
+    {
+        _pCM = FindAnyObjectByType<LeverCodeManager>();
+        animPalanca = GetComponentInChildren<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canInteract == true)
+        {
+            isActive = true;
+            animPalanca.SetBool("LeverUp", true);
+            foreach (var palanquitauwu in palancaDesactivar)
+            {
+                palanquitauwu.desactivar();
+            }
+
+            if (isActive == true)
+            {
+                foreach (var palanquitauwu in palancaActivar)
+                {
+                    palanquitauwu.activar();
+                }
+
+            }
+        }
+    }
+    public void desactivar()
+    {
+        isActive = false;
+        animPalanca.SetBool("LeverUp", false);
+    }
+    public void activar()
+    {
+        isActive = true;
+        animPalanca.SetBool("LeverUp", true);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canInteract = true;
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canInteract = false;
+        }
+    }
+}
